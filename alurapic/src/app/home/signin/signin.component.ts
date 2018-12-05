a2ff368d4c68e7d5065a790cb38d4a836c73d4c4
Paty Alves <patriciaalves10@gmail.com>
2018-12-05 21:33:12 -0200

    sending credential to API

--- alurapic/src/app/home/signin/signin.component.ts
+++ alurapic/src/app/home/signin/signin.component.ts
@@ -1,19 +1,35 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { AuthService } from "src/app/core/auth.service";
 
 @Component({
-    templateUrl:'./signin.component.html'
+    templateUrl: './signin.component.html'
 })
-export class SignInComponent implements OnInit{
-    
-    loginForm:FormGroup;
-    
-    constructor(private formBuilde:FormBuilder){}
-    
+export class SignInComponent implements OnInit {
+
+    loginForm: FormGroup;
+
+    constructor(private formBuilde: FormBuilder,
+        private auth: AuthService) { }
+
     ngOnInit(): void {
-       this.loginForm=this.formBuilde.group({
-           userName:['' , Validators.required],
-           password:['', Validators.required]
-       });
+        this.loginForm = this.formBuilde.group({
+            userName: ['', Validators.required],
+            password: ['', Validators.required]
+        });
+    }
+
+    doLogin() {
+        let userName=this.loginForm.get('userName').value;
+        let password=this.loginForm.get('password').value;
+
+        this.auth.authenticate(userName, password)
+            .subscribe(
+                res => console.log('sucess ', res),
+                err => {
+                    console.log('error ', err)
+                    this.loginForm.reset();
+                }
+            );
     }
 }
